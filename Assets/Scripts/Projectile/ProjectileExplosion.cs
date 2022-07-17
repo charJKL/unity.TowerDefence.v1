a1eb3d2b2794c35804d81de53a024df7350acfef
charJKL <11600464+charJKL@users.noreply.github.com>
2022-07-17 11:58:48 +0200

    Add explosion effect after hit.

---
+++ Assets/Scripts/Projectile/ProjectileExplosion.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Projectile))]
+public class ProjectileExplosion : MonoBehaviour
+{
+	[SerializeField] private GameObject explosion;
+	
+	const int LAYER_ENEMY = 1 << 6;
+	private Projectile projectile;
+	private new Collider collider;
+	
+	private void Awake()
+	{
+		projectile = GetComponent<Projectile>();
+		collider = GetComponent<Collider>();
+	}
+	
+	private void OnTriggerEnter(Collider other)
+	{
+		if(other.transform == projectile.Target)
+		{
+			if(explosion) Instantiate(explosion, transform.position, Quaternion.identity);
+			Collider[] colliders = Physics.OverlapSphere(other.transform.position, projectile.Range, LAYER_ENEMY);
+			foreach(Collider collider in colliders)
+			{
+				Destroy(collider.gameObject);
+			}
+			Destroy(gameObject);
+		}
+	}
+}

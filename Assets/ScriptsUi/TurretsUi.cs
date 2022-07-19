061d217ce3bbf263174f21a360256e8cf057b5b0
charJKL <11600464+charJKL@users.noreply.github.com>
2022-07-19 12:31:18 +0200

    Add `TurretLaser` type.

--- Assets/ScriptsUi/TurretsUi.cs
+++ Assets/ScriptsUi/TurretsUi.cs
@@ -10,10 +10,12 @@ public class TurretsUi : MonoBehaviour
 	[SerializeField] private Player player;
 	[SerializeField] private TurretRecord turretGun;
 	[SerializeField] private TurretRecord turretLauncher;
+	[SerializeField] private TurretRecord turretLaser;
 	
 	// TODO this should be some kind of list:
 	[SerializeField] private TurretSelectionUi turretGunButton;
 	[SerializeField] private TurretSelectionUi turretLauncherButton;
+	[SerializeField] private TurretSelectionUi turretLaserButton;
 	
 	public void Start()
 	{
@@ -23,6 +25,7 @@ public class TurretsUi : MonoBehaviour
 		
 		turretGunButton.SetCost(turretGun.Cost);
 		turretLauncherButton.SetCost(turretLauncher.Cost);
+		turretLaserButton.SetCost(turretLaser.Cost);
 	}
 	
 	public void SelectTurretGun()
@@ -45,21 +48,34 @@ public class TurretsUi : MonoBehaviour
 		constructionManager.SelectTurret(turretLauncher);
 	}
 	
+	public void SelectTurretLaser()
+	{
+		if(turretLaser.Cost > player.Money)
+		{
+			turretLaserButton.InsufficientFund();
+			return;
+		}
+		constructionManager.SelectTurret(turretLaser);
+	}
+	
 	private void CheckMoneyCosts(float money)
 	{
 		turretGunButton.CanAfford(turretGun.Cost <= money);
 		turretLauncherButton.CanAfford(turretLauncher.Cost <= money);
+		turretLaserButton.CanAfford(turretLaser.Cost <= money);
 	}
 	
 	private void HighlightInsufficientFund(TurretRecord turretRecord)
 	{
 		if(turretRecord.Turret == turretGun.Turret) turretGunButton.InsufficientFund();
 		if(turretRecord.Turret == turretLauncher.Turret) turretLauncherButton.InsufficientFund();
+		if(turretRecord.Turret == turretLaser.Turret) turretLaserButton.InsufficientFund();
 	}
 	
 	private void HighlightCurrentSelection(TurretRecord turretRecord)
 	{
 		turretGunButton.IsSelected(turretRecord.Turret == turretGun.Turret);
 		turretLauncherButton.IsSelected(turretRecord.Turret == turretLauncher.Turret);
+		turretLaserButton.IsSelected(turretRecord.Turret == turretLaser.Turret);
 	}
 }

2399357dddcee9685da6e2850f81bdffeff79b7a
charJKL <11600464+charJKL@users.noreply.github.com>
2022-07-20 09:12:28 +0200

    Add laser vfx.

--- Assets/Scripts/Projectile/ProjectileBeam.cs
+++ Assets/Scripts/Projectile/ProjectileBeam.cs
@@ -1,10 +1,28 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
+
+public class Debug2
+{
+	static public void Point(Vector3 position)
+	{
+		Vector3 start = position + Vector3.up;
+		Debug.DrawLine(start, position, Color.red, 5f);
+	}
+	
+	static public void Direction(Vector3 position, Vector3 direction)
+	{
+		Vector3 end = position + direction.normalized;
+		Vector3 mark = end + Vector3.up * .5f;
+		Debug.DrawLine(position, end, Color.red, 5f);
+		Debug.DrawLine(mark, end, Color.red, 5f);
+	}
+	
+}
 
 [RequireComponent(typeof(LineRenderer))]
 public class ProjectileBeam : MonoBehaviour
 {
+	[SerializeField] private ParticleSystem vfx;
+	
 	private Transform barrel;
 	private Transform target;
 	private LineRenderer line;
@@ -14,6 +32,7 @@ public class ProjectileBeam : MonoBehaviour
 	private void Awake()
 	{
 		line = GetComponent<LineRenderer>();
+		vfx.Play();
 	}
 	
 	public ProjectileBeam Init(Transform transform)
@@ -29,6 +48,12 @@ public class ProjectileBeam : MonoBehaviour
 	{
 		line.SetPosition(0, barrel.position);
 		line.SetPosition(1, target.position);
+		
+		Vector3 direction = barrel.position - target.position;
+		Vector3 point = target.position + direction.normalized;
+		vfx.transform.position = point;
+		vfx.transform.rotation = Quaternion.LookRotation(direction);
+		Debug2.Point(point);
 	}
 	
 	public void SetBarrel(Transform transform)
@@ -44,10 +69,12 @@ public class ProjectileBeam : MonoBehaviour
 	public void Enable()
 	{
 		gameObject.SetActive(true);
+		
 	}
 	
 	public void Disable()
 	{
 		gameObject.SetActive(false);
+		//vfx.Stop();
 	}
 }
